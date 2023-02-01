10421036fe662721733c979f2b85214dda5f3a26
John Jared Creech <john.creech@colorado.edu>
2023-02-01 11:01:22 -0700

    changed shebang from bash to sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -32,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat ../conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -48,23 +48,25 @@ then
 		exit 1
 	fi
 fi
+echo "Removing the old writer utility and compiling as a native application"
+make clean
 
 for i in $( seq 1 $NUMFILES)
 do
 	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING="./finder.sh ${WRITEDIR} ${WRITESTR}"
+OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
 set +e
-echo "$OUTPUTSTRING" | grep "$WRITESTR"
+echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
 	echo "success"
 	exit 0
 else
-	echo "failed: expected ${WRITESTR} but instead found ${OUTPUTSTRING}"
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
 	exit 1
 fi
